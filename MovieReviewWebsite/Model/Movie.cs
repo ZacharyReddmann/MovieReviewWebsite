df9689785e5f74920055b3adb58623cb06953488
Zariak718 <74118623+Zariak718@users.noreply.github.com>
2021-10-02 15:19:47 -0400

    Created the database, created model folder with appDbContext  and movie class in it

---
+++ MovieReviewWebsite/Model/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieReviewWebsite.Model
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] //Display attribute requiring name to not be null
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
